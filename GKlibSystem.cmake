6d1acfedc4b4078a279e39747e1e52a533d0e421
Sai Krishna <sirumalla.s@husky.neu.edu>
2020-10-14 10:00:50 -0400

    change gcc flags

--- GKlibSystem.cmake
+++ GKlibSystem.cmake
@@ -29,7 +29,7 @@ endif(CYGWIN)
 if(CMAKE_COMPILER_IS_GNUCC)
 # GCC opts.
   set(GKlib_COPTIONS "${GKlib_COPTIONS} -std=c99 -fno-strict-aliasing")
-  set(GKlib_COPTIONS "${GKlib_COPTIONS} -march=native")
+  set(GKlib_COPTIONS "${GKlib_COPTIONS} -mcpu=native")
   if(NOT MINGW)
       set(GKlib_COPTIONS "${GKlib_COPTIONS} -fPIC")
   endif(NOT MINGW)
